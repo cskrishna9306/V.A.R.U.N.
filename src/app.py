aa7bcdfad00009522756ed4c0066fc765c72f6ca
Sai Chaparala <cskrishna9306@gmail.com>
2026-02-27 15:51:44 -0800

    feat(api): New endpoint to query llm

--- src/app.py
+++ src/app.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from gemini import client
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"status": "Success", "message": "Connection to varun.saichaparala.com is working!"}
+
+@app.post("/talk")
+def talk(
+    prompt: str
+):
+    response = client.models.generate_content(
+        model="models/gemini-3-flash-preview",
+        contents=prompt
+    )
+    return {"response": response.text}
+    
