05def7c34ddd40518c057935a1fe2b796f34e900
Sai Chaparala <cskrishna9306@gmail.com>
2026-03-02 23:48:14 -0800

    feat(utils): Created, a get_gif() routine to query GIPHY API for GIFs

--- src/llm/config.py
+++ src/llm/config.py
@@ -1,3 +1,10 @@
+# Import standard packages
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
 # Defines the LLM to use
 MODEL_ID = "qwen2.5:latest"
 
@@ -9,3 +16,17 @@ config_list = [
     # "api_key": "ollama",
   }
 ]
+
+# Load the GIPHY API key
+GIPHY_API_KEY = os.getenv("GIPHY_API_KEY")
+
+# Instantiate the GIPHY Base URL
+GIPHY_BASE_SEARCH_URL = "https://api.giphy.com/v1/gifs/search"
+
+# Define the number of GIFs to query for
+GIPHY_SEARCH_LIMIT = 20
+
+# Define the chance of querying GIPHY
+# We retrieve GIFs only when the random number falls within this limit
+# Higher the value, greater the chance of generating a GIF
+GIF_LOTTERY = 0.5
--- src/llm/utils.py
+++ src/llm/utils.py
@@ -1,4 +1,19 @@
+# Import standard packages
+import random
+import requests
+import python_weather
 from importlib.resources import files
+
+# Import custom packages
+from src.models import (
+    Weather
+)
+from src.llm.config import (
+    GIPHY_API_KEY,
+    GIPHY_BASE_SEARCH_URL,
+    GIPHY_SEARCH_LIMIT,
+    GIF_LOTTERY,
+)
 
 def load_system_prompt(prompt_file: str) -> str | None:
     """
@@ -20,3 +35,82 @@ def load_system_prompt(prompt_file: str) -> str | None:
         print(f"Error loading prompt file {prompt_file}: {e}")
     
     return None
+
+async def get_weather(city: str, country: str | None) -> Weather | None:
+    """
+    Function to fetch the current weather for a given location using the python_weather package.
+
+    Args:
+        city (str): The city for which to fetch the weather.
+        country (str | None): The country for which to fetch the weather.
+    """
+    try:
+        # Declare the client. The measuring unit used defaults to the metric system (celcius, km/h, etc.)
+        async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
+            
+            # Fetch a weather forecast from the specified location.
+            weather = await client.get(city)
+            
+            return Weather(
+                temperature=weather.temperature,
+                condition=weather.description
+            )
+    
+    except Exception as e:
+        print(f"Error: {e}")
+    
+    return
+
+def get_gif(query: str) -> str | None:
+    """
+    Retrieves a randomly selected GIF URL from GIPHY matching the provided search query.
+
+    Args:
+        query (str): The search query to retrieve GIFs for
+
+    Returns:
+        str: The URL to the retrieved GIF
+    """
+    
+    # Check to see if we should trigger GIPHY
+    if random.random() < GIF_LOTTERY:
+        return
+    
+    try:
+        # Construct the query parameters
+        params = {
+            "api_key": GIPHY_API_KEY,
+            "q": query,
+            "limit": GIPHY_SEARCH_LIMIT
+        }
+        
+        # Query the GIPHY Search API endpoint
+        response = requests.get(
+            url=GIPHY_BASE_SEARCH_URL,
+            params=params
+        )
+
+        # Check for valid successful status
+        if response.status_code != 200:
+            print(f"Error: {response.status_code}")
+            return
+        
+        # JSONIFY the response data for easy parsing
+        data = response.json().get("data", [])
+        
+        # Check for empty GIFs
+        if not data:
+            print("Error: No GIFs were returned")
+            return
+        
+        # Retrieve ONLY the GIF URLs from the response
+        # TODO: Need to type check for output validation under each GIF
+        GIF_URLS = [gif["images"]["original"]["url"] for gif in data]
+        
+        # Rgeturn a randomly picked GIF from the response
+        return random.choice(GIF_URLS)
+    
+    except Exception as e:
+        print(f"Error: {e}")
+    
+    return
