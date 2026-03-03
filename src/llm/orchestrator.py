094400843458ab3ba2c37406202332f0aa8f2818
Sai Chaparala <cskrishna9306@gmail.com>
2026-03-02 23:46:53 -0800

    feat(llm): Now, the agent can take in generic pydantic models and model the output accordingly

--- src/llm/orchestrator.py
+++ src/llm/orchestrator.py
@@ -1,5 +1,6 @@
 # Import standard packages
 import asyncio
+from pydantic import BaseModel
 
 # Import autogen packages
 from autogen_agentchat.agents import AssistantAgent
@@ -9,18 +10,25 @@ from autogen_agentchat.ui import Console
 # Import custom packages
 from src.llm.config import MODEL_ID
 from src.llm.utils import load_system_prompt
+from src.models import YapResponse
 
 class V_A_R_U_N(AssistantAgent):
     """
     The brain behind the V.A.R.U.N. discord bot.
     """
         
-    def __init__(self, tools: list | None):
+    def __init__(self, tools: list | None, response_format: BaseModel | None = YapResponse):
         super().__init__(
             name="V_A_R_U_N",
             system_message=load_system_prompt("V-A-R-U-N.md"),
             model_client=OllamaChatCompletionClient(
-                model=MODEL_ID
+                model=MODEL_ID,
+                response_format=response_format,
+                model_info={
+                    "vision": False,
+                    "function_calling": True,
+                    "json_output": True,
+                }
             ),
             tools=tools
         )
